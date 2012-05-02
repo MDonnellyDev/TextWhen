0b20af410a3750e313ee5e1a419454ad8c576158
Matt Donnelly <mdonnelly.dev@gmail.com>
2012-05-02 01:49:39 -0400

    Add sent/delivery Pending Intents. Customize some Toast messages based on status.

--- src/com/mddev/l8text/SmsService.java
+++ src/com/mddev/l8text/SmsService.java
@@ -1,11 +1,20 @@
 package com.mddev.l8text;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class SmsService extends Service {
+
+	public static final String	SMS_SENT			= "SMS_SENT";
+	public static final String	SMS_DELIVERED	= "SMS_DELIVERED";
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
@@ -20,11 +29,61 @@ public class SmsService extends Service {
 
 		SmsManager sms = SmsManager.getDefault();
 
+		PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+				SMS_SENT), 0);
+
+		PendingIntent deliveryIntent = PendingIntent.getBroadcast(this, 0,
+				new Intent(SMS_DELIVERED), 0);
+
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+					case Activity.RESULT_OK:
+						Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_SHORT)
+								.show();
+						break;
+					case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+						Toast.makeText(getBaseContext(), "SMS failed to be sent",
+								Toast.LENGTH_SHORT).show();
+						break;
+					case SmsManager.RESULT_ERROR_NO_SERVICE:
+						Toast.makeText(getBaseContext(), "No service", Toast.LENGTH_SHORT)
+								.show();
+						break;
+					case SmsManager.RESULT_ERROR_NULL_PDU:
+						Toast.makeText(getBaseContext(), "Null PDU", Toast.LENGTH_SHORT)
+								.show();
+						break;
+					case SmsManager.RESULT_ERROR_RADIO_OFF:
+						Toast.makeText(getBaseContext(), "Radio off", Toast.LENGTH_SHORT)
+								.show();
+						break;
+				}
+			}
+		}, new IntentFilter(SMS_SENT));
+
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+					case Activity.RESULT_OK:
+						Toast.makeText(getBaseContext(), "SMS delivered",
+								Toast.LENGTH_SHORT).show();
+						break;
+					case Activity.RESULT_CANCELED:
+						Toast.makeText(getBaseContext(), "SMS delivery failed",
+								Toast.LENGTH_SHORT).show();
+						break;
+				}
+			}
+		}, new IntentFilter(SMS_DELIVERED));
+
+		int i = 0;
 		for (String recipient : text.getRecipient().split(",")) {
-			sms.sendTextMessage(recipient, null, text.getMessageContent(), null, null);
+			sms.sendTextMessage(recipient, null, text.getMessageContent(),
+					sentIntent, deliveryIntent);
 		}
-		// sms.sendTextMessage(text.getRecipient(), null, text.getMessageContent(),
-		// null, null);
 
 		return Service.START_NOT_STICKY;
 
