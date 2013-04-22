8c1a58694b5a1169febb74ece47044e067b0f400
Matt Donnelly <mdonnelly.dev@gmail.com>
2013-04-21 23:07:28 -0400

    Add DatabaseUtils to escape SQL so that it does not crash due to single quotes/apostrophes.

--- src/com/mtd/textwhen/TextDbAdapter.java
+++ src/com/mtd/textwhen/TextDbAdapter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
@@ -15,53 +16,44 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.util.Log;
 
 public class TextDbAdapter {
-	public static int						KEY_COUNT							= 0;
-	private static final String	DB_NAME								= "TextData.db";
-	private static final String	DB_TABLE							= "TextTable";
-	private static final int		DB_VERSION						= 1;
+	public static int KEY_COUNT = 0;
+	private static final String DB_NAME = "TextData.db";
+	private static final String DB_TABLE = "TextTable";
+	private static final int DB_VERSION = 1;
 
-	private static final String	KEY_ID								= "_id";
-	private static final int		KEY_COLUMN						= 0;
+	private static final String KEY_ID = "_id";
+	private static final int KEY_COLUMN = 0;
 
-	private static final String	KEY_RECIPIENT					= "Recipient";
-	private static final int		RECIPIENT_COLUMN			= 1;
+	private static final String KEY_RECIPIENT = "Recipient";
+	private static final int RECIPIENT_COLUMN = 1;
 
-	private static final String	KEY_SUBJECT						= "Subject";
-	private static final int		SUBJECT_COLUMN				= 2;
+	private static final String KEY_SUBJECT = "Subject";
+	private static final int SUBJECT_COLUMN = 2;
 
-	private static final String	KEY_BODY							= "Body";
-	private static final int		BODY_COLUMN						= 3;
+	private static final String KEY_BODY = "Body";
+	private static final int BODY_COLUMN = 3;
 
-	private static final String	KEY_SCHEDULED					= "ScheduleTime";
-	private static final int		SCHEDULEDTIME_COLUMN	= 4;
+	private static final String KEY_SCHEDULED = "ScheduleTime";
+	private static final int SCHEDULEDTIME_COLUMN = 4;
 
-	private static final String	KEY_MODIFIED					= "ModifiedTime";
-	private static final int		MODIFIEDTIME_COLUMN		= 5;
+	private static final String KEY_MODIFIED = "ModifiedTime";
+	private static final int MODIFIEDTIME_COLUMN = 5;
 
-	private static final String	KEY_GMT_OFFSET				= "GmtOffset";
-	private static final int		GMT_COLUMN						= 6;
+	private static final String KEY_GMT_OFFSET = "GmtOffset";
+	private static final int GMT_COLUMN = 6;
 
-	private static final String	DB_CREATE							= (new StringBuilder()
-																												.append("create table ")
-																												.append(DB_TABLE)
-																												.append(" (")
-																												.append(KEY_ID)
-																												.append(
-																														" integer primary key autoincrement, ")
-																												.append(KEY_RECIPIENT).append(
-																														" text not null, ").append(
-																														KEY_SUBJECT).append(" text, ")
-																												.append(KEY_BODY).append(
-																														" text not null, ").append(
-																														KEY_SCHEDULED).append(" long, ")
-																												.append(KEY_MODIFIED)
-																												.append(" long, ").append(
-																														KEY_GMT_OFFSET).append(" long);"))
-																												.toString();
+	private static final String DB_CREATE = (new StringBuilder()
+			.append("create table ").append(DB_TABLE).append(" (")
+			.append(KEY_ID).append(" integer primary key autoincrement, ")
+			.append(KEY_RECIPIENT).append(" text not null, ")
+			.append(KEY_SUBJECT).append(" text, ").append(KEY_BODY)
+			.append(" text not null, ").append(KEY_SCHEDULED).append(" long, ")
+			.append(KEY_MODIFIED).append(" long, ").append(KEY_GMT_OFFSET)
+			.append(" long);")).toString();
 
-	private SQLiteDatabase			db;
-	private final Context				context;
-	private TextOpenHelper			dbHelper;
+	private SQLiteDatabase db;
+	private final Context context;
+	private TextOpenHelper dbHelper;
 
 	public TextDbAdapter(Context context) {
 		this.context = context;
@@ -83,11 +75,12 @@ public class TextDbAdapter {
 
 	public long insertEntry(OutgoingText text) {
 		StringBuilder whereBuilder = new StringBuilder();
-		whereBuilder.append(KEY_RECIPIENT).append(" = \"").append(text.getRecipient());
-		whereBuilder.append("\" and ").append(KEY_BODY).append(" = \"").append(
-				text.getMessageContent()).append("\"");
-		Cursor cursor = db.query(DB_TABLE, new String[] { KEY_ID }, whereBuilder.toString(), null,
-				null, null, null);
+		whereBuilder.append(KEY_RECIPIENT).append(" = \"")
+				.append(text.getRecipient());
+		whereBuilder.append("\" and ").append(KEY_BODY).append(" = \"")
+				.append(text.getMessageContent()).append("\"");
+		Cursor cursor = db.query(DB_TABLE, new String[] { KEY_ID },
+				whereBuilder.toString(), null, null, null, null);
 		if (cursor.moveToFirst()) {
 			cursor.close();
 			return -1;
@@ -115,12 +108,18 @@ public class TextDbAdapter {
 	}
 
 	public long getEntryRow(OutgoingText text) {
-		String where = KEY_RECIPIENT + " = '" + text.getRecipient() + "' AND " + KEY_SUBJECT
-				+ "= '" + text.getSubject() + "' OR " + KEY_SUBJECT + " IS NULL" + " AND " + KEY_BODY
-				+ "= '" + text.getMessageContent() + "' AND " + KEY_SCHEDULED + "= '"
+		String where = KEY_RECIPIENT + " = '" + text.getRecipient() + "' AND "
+				+ KEY_SUBJECT + "= '" + text.getSubject() + "' OR "
+				+ KEY_SUBJECT + " IS NULL" + " AND " + KEY_BODY + "= '"
+				+ text.getMessageContent() + "' AND " + KEY_SCHEDULED + "= '"
 				+ text.getScheduledDateAsLong() + "'";
-		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID }, where, null, null, null,
-				null, null);
+
+		StringBuilder sb = new StringBuilder();
+
+		DatabaseUtils.appendEscapedSQLString(sb, where);
+
+		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID },
+				sb.toString(), null, null, null, null, null);
 
 		if (cursor.moveToFirst()) {
 			Long row = Long.parseLong(cursor.getString(0));
@@ -137,15 +136,17 @@ public class TextDbAdapter {
 	}
 
 	public Cursor getAllEntries() {
-		return db.query(DB_TABLE, new String[] { KEY_ID, KEY_RECIPIENT, KEY_SUBJECT, KEY_BODY,
-				KEY_SCHEDULED, KEY_MODIFIED, KEY_GMT_OFFSET }, null, null, null, null, null);
+		return db.query(DB_TABLE, new String[] { KEY_ID, KEY_RECIPIENT,
+				KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED, KEY_MODIFIED,
+				KEY_GMT_OFFSET }, null, null, null, null, null);
 	}
 
 	public List<OutgoingText> getAllEntriesList() {
 		List<OutgoingText> textList = new ArrayList<OutgoingText>();
-		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID, KEY_RECIPIENT,
-				KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED, KEY_MODIFIED, KEY_GMT_OFFSET }, null, null,
-				null, null, KEY_SCHEDULED, null);
+		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID,
+				KEY_RECIPIENT, KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED,
+				KEY_MODIFIED, KEY_GMT_OFFSET }, null, null, null, null,
+				KEY_SCHEDULED, null);
 		if (cursor.moveToFirst()) {
 			do {
 				String recipient = cursor.getString(RECIPIENT_COLUMN);
@@ -157,8 +158,8 @@ public class TextDbAdapter {
 				modified.setTimeInMillis(cursor.getLong(MODIFIEDTIME_COLUMN));
 				long gmtOffset = cursor.getInt(GMT_COLUMN);
 				try {
-					textList.add(new OutgoingText(recipient, subject, body, scheduled, modified,
-							gmtOffset));
+					textList.add(new OutgoingText(recipient, subject, body,
+							scheduled, modified, gmtOffset));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -172,9 +173,10 @@ public class TextDbAdapter {
 	}
 
 	public Cursor setCursorToEntry(long rowIndex) {
-		Cursor result = db.query(true, DB_TABLE, new String[] { KEY_ID, KEY_RECIPIENT,
-				KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED, KEY_MODIFIED }, KEY_ID + "=" + rowIndex, null,
-				null, null, null, null);
+		Cursor result = db.query(true, DB_TABLE, new String[] { KEY_ID,
+				KEY_RECIPIENT, KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED,
+				KEY_MODIFIED }, KEY_ID + "=" + rowIndex, null, null, null,
+				null, null);
 		if (result.getCount() == 0 || !result.moveToFirst()) {
 			throw new SQLException("No entry found for row " + rowIndex);
 		} else {
@@ -183,9 +185,10 @@ public class TextDbAdapter {
 	}
 
 	public OutgoingText getEntry(long rowIndex) {
-		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID, KEY_RECIPIENT,
-				KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED, KEY_MODIFIED }, KEY_ID + "=" + rowIndex, null,
-				null, null, null, null);
+		Cursor cursor = db.query(true, DB_TABLE, new String[] { KEY_ID,
+				KEY_RECIPIENT, KEY_SUBJECT, KEY_BODY, KEY_SCHEDULED,
+				KEY_MODIFIED }, KEY_ID + "=" + rowIndex, null, null, null,
+				null, null);
 		if (cursor.getCount() == 0 || !cursor.moveToFirst()) {
 			throw new SQLException("No entry found for row " + rowIndex);
 		} else {
@@ -199,7 +202,8 @@ public class TextDbAdapter {
 			long gmtOffset = cursor.getInt(GMT_COLUMN);
 			try {
 				cursor.close();
-				return new OutgoingText(recipient, subject, body, scheduled, modified, gmtOffset);
+				return new OutgoingText(recipient, subject, body, scheduled,
+						modified, gmtOffset);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -216,7 +220,8 @@ public class TextDbAdapter {
 
 	private static class TextOpenHelper extends SQLiteOpenHelper {
 
-		public TextOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		public TextOpenHelper(Context context, String name,
+				CursorFactory factory, int version) {
 			super(context, name, factory, version);
 		}
 
@@ -227,7 +232,8 @@ public class TextDbAdapter {
 
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			Log.w("DbAdapter", "Upgrading from version " + oldVersion + " to version " + newVersion
+			Log.w("DbAdapter", "Upgrading from version " + oldVersion
+					+ " to version " + newVersion
 					+ ", which will destroy all old data.");
 			db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
 			onCreate(db);
